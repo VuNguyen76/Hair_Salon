a4eb81e73b11bd6ecc832b585f7dbd4efc905185
Nguyen Thanh Vu <thanhvuttq@gmail.com>
2024-12-29 09:02:52 +0700

    Saving changes before rebase

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'app/static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/static/images' )
 
 # settings.py
 LOGOUT_REDIRECT_URL = '/'  # Hoặc URL bạn muốn người dùng được chuyển hướng sau khi logout
