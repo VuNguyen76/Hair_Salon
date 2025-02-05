9a33b4d46ae93a91cde59da1fae86b3c48b8f163
vanlin-05 <nguyenvanlien.dt2021@gmail.com>
2025-02-05 11:27:42 +0700

    Xử lý chức năng tích điểm cho khách hàng sử dụng dịch vụ

--- HairSalon/staff/views.py
+++ HairSalon/staff/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from booking.models import Booking
 from django.core.paginator import Paginator
+from decimal import Decimal
+
 
 def staff(request):
     bookings_list = Booking.objects.all().order_by('-booking_date')
@@ -31,6 +33,13 @@ def staff_booking_single(request, booking_id):
         booking.status = new_status
         if new_status == 'completed':
             booking.revenue = booking.total_price()  # Cập nhật doanh thu nếu trạng thái là "Hoàn thành"
+            reward_points = booking.revenue * Decimal('0.1')
+            
+            # Cập nhật số điểm thưởng cho khách hàng
+            if booking.user:
+                user_profile = booking.user.infor  # Lấy thông tin người dùng
+                user_profile.reward_points += reward_points  # Thêm điểm thưởng
+                user_profile.save()
         booking.save()
 
         return redirect('staff')  # Quay lại trang danh sách sau khi cập nhật
@@ -47,6 +56,15 @@ def staff_booking_multiple(request):
                 booking.status = new_status
                 if new_status == 'completed':
                     booking.revenue = booking.total_price()  # Cập nhật doanh thu
+                    # Tính điểm thưởng (10% số tiền chi tiêu)
+                    reward_points = booking.revenue * Decimal('0.1')
+                    
+                    # Cập nhật số điểm t    hưởng cho khách hàng
+                    if booking.user:
+                        user_profile = booking.user.infor
+                        user_profile.reward_points += reward_points
+                        user_profile.save()
+ 
                 booking.save()
 
         return redirect('staff')  # Quay lại trang danh sách sau khi cập nhật
