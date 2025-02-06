449f5cd5120b47a88d4e10f7c76a753d74b9e125
quoctinh516725 <tinhnq516725@gmail.com>
2025-02-06 16:20:54 +0700

    Cập nhật sửa lỗi

--- HairSalon/management/urls.py
+++ HairSalon/management/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
    path('manage/', views.manage_dashboard, name='manage'),
+   path('manage_service/', views.manage_service, name='manage_service'),
 ]
